a714d598c0cd106ed8a61e7ed96d203e60b7dd4c
invisiblegeek1 <appumoorthy15@gmail.com>
2021-08-04 17:32:53 +0530

    transaction server initial commit

---
+++ TransactionServer/src/main/java/com/example/TransactionServer/TransactionServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.TransactionServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionServerApplication.class, args);
+	}
+
+}
