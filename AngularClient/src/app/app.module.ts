14e94d83d065370b33aa0d7205fa974eb2d91acb
pushparaja <appumoorthy15@gmail.com>
2021-08-05 13:27:25 +0000

    home and form section created

--- AngularClient/src/app/app.module.ts
+++ AngularClient/src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { UserFormComponent } from './user-form/user-form.component';
+import { HomeSectionComponent } from './home-section/home-section.component';
+import { FormSectionComponent } from './form-section/form-section.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserFormComponent
+    UserFormComponent,
+    HomeSectionComponent,
+    FormSectionComponent,
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
